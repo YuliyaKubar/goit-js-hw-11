54d5974caf55baf24c3d597b2fe929bcf339a0ee
YuliyaKubar <113610848+YuliyaKubar@users.noreply.github.com>
2023-04-08 13:55:41 +0300

    function fetchImages

--- src/fetchImages.js
+++ src/fetchImages.js
@@ -11,6 +11,7 @@ const lightbox = new SimpleLightbox('.gallery a', {
   captionsData: 'alt',
   captionDelay: 250,
 });
+
 let currentPage = 1;
 
 btnLoadMore.addEventListener('click', onLoadMore);
@@ -18,23 +19,24 @@ btnLoadMore.addEventListener('click', onLoadMore);
 export async function fetchImages(url) {
   try {
     const response = await axios(url);
-    const hits = response.data;
-    galleryEl.insertAdjacentHTML('beforeend', renderImageGallery(hits));
+    const data = response.data;
+    galleryEl.insertAdjacentHTML('beforeend', renderImageGallery(data));
     currentPage += 1;
     btnLoadMore.classList.remove('is-hidden');
     lightbox.refresh();
-    return hits;
+
+    if (currentPage === Math.ceil(data.totalhits / 40)) {
+      btnLoadMore.style.display = 'none';
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    }
+
+    return data;
   } catch {
     btnLoadMore.classList.add('is-hidden');
     Notify.failure(
       "We're sorry, but you've reached the end of search results."
     );
   }
-
-  // if (url.hits.length === totalhits) {
-  //   btnLoadMore.style.display = 'none';
-  //   Notify.failure(
-  //     "We're sorry, but you've reached the end of search results."
-  //   );
-  // }
 }
